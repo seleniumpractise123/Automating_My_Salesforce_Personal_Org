d1a6763d25db8aba82a557ccde894a0f73ec8603
Naveen Ashok <naveensalesforce.learning70@gmail.com>
2025-12-25 19:20:22 +0530

    added half of this video functionality ParallelTest_CrossBrowserParameters_MultiEnvironmentSetup

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriveryFactory;
@@ -28,10 +29,16 @@ public class BaseTest {
 	protected Properties prop;
 	protected SoftAssert softAssert;
 	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browserName) {
 		df = new DriveryFactory();
 		prop = df.initProp();
+		
+		if(browserName != null) {
+			prop.setProperty("browser", browserName);
+		}
+		
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 		softAssert = new SoftAssert();
